0f36213ba747841b388ee518783f1bd39db5dd47
Fernando Frazão da Silva <ffrazao@gmail.com>
2019-05-15 23:09:55 -0300

    Apresentação
    Inclusão do Bootstrap 4
    Favicon
    
    Signed-off-by: Fernando Frazão da Silva <ffrazao@gmail.com>

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pag/login/login.component';
+import { UsuarioComponent } from './pag/usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "usuario", component: UsuarioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
