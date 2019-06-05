af3477f344c6bd071913b639519ee9595a8b1a36
Fernando Frazão da Silva <ffrazao@gmail.com>
2019-06-04 22:35:30 -0300

    Inicio da construção da API
    Signed-off-by: Fernando Frazão da Silva <ffrazao@gmail.com>

---
+++ api/comum/src/test/java/br/gov/df/emater/comum/FuncoesTest.java
@@ -0,0 +1,7 @@
+package br.gov.df.emater.comum;
+
+import junit.framework.TestCase;
+
+public class FuncoesTest extends TestCase {
+
+}
---
+++ api/repositorio-principal/src/main/java/br/gov/df/emater/repositorio_principal/RepositorioPrincipalApplication.java
@@ -0,0 +1,13 @@
+package br.gov.df.emater.repositorio_principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioPrincipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioPrincipalApplication.class, args);
+	}
+
+}
