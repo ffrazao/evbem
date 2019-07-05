75f3dad918344bcfeb1a210cc43f128e16aa746d
Fernando Frazao da Silva <ffrazao@gmail.com>
2019-07-05 16:49:37 -0300

    inclusão do componente crud
    Signed-off-by: Fernando Frazao da Silva <ffrazao@gmail.com>

---
+++ web/src/app/comum/componente/crud-component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({})
+export class CrudComponent  implements OnInit {
+
+  // seleçao
+  public sub;
+  public ids: number[];
+  public pos: number;
+  public novaPos: number;
+
+  constructor(
+    protected _httpClient: HttpClient,
+    protected _router: Router,
+    protected _activatedRoute: ActivatedRoute,
+    private _urlPrincipal: string[]
+  ) {
+      console.log(1);
+  }
+
+  ngOnInit(): void {
+    this.sub = this._activatedRoute.paramMap.subscribe(params => {
+      let temp: string;
+      temp = params.get('ids');
+      if (!(/n/i).test(temp)) {
+        try {
+          this.ids = temp.split(',').map(e => Number.parseInt(e, 10));
+          temp = params.get('pos');
+          this.setPos(Number.parseInt(temp, 10));
+        } catch (e) {
+        }
+      }
+      if (!this.ids) {
+        this._router.navigate(this._urlPrincipal);
+      }
+      if (!this.pos) {
+        this.setPos(0);
+      }
+      this.novaPos = this.pos + 1;
+    });
+  }
+  
+  get id() {
+    if (!this.ids || !Array.isArray(this.ids)) return null;
+    return this.ids[this.pos];
+  }
+
+  public vaiParaPrimeiro(): void {
+    this.vaiPara(0);
+  }
+
+  public vaiParaAnterior(): void {
+    this.vaiPara(this.pos - 1);
+  }
+
+  public vaiParaProximo(): void {
+    this.vaiPara(this.pos + 1);
+  }
+
+  public vaiParaUltimo(): void {
+    this.vaiPara(this.ids.length - 1);
+  }
+
+  public vaiPara(pos): void {
+    pos = ("" + pos).trim();
+    if ((pos || pos == 0) && !isNaN(pos)) {
+      let url = this._urlPrincipal.slice(0);
+      url.push(this.ids.join());
+      url.push(pos);
+      this._router.navigate(url);
+    }
+  }
+
+  private setPos(vlr: number) {
+    if (vlr < 0 || !this.ids || !this.ids.length) {
+      vlr = 0;
+    } else if (vlr >= this.ids.length) {
+      vlr = this.ids.length -1;
+    }
+    this.pos = vlr;
+  }
+
+}
--- web/src/app/pag/usuario/usuario.component.ts
+++ web/src/app/pag/usuario/usuario.component.ts
@@ -1,7 +1,10 @@
-import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Usuario } from './usuario-list.component';
 import { ActivatedRoute, Router } from '@angular/router';
+
+import { CrudComponent } from 'src/app/comum/componente/crud-component';
+import { Usuario } from 'src/app/pag/usuario/usuario-list.component';
+
 
 
 @Component({
@@ -9,92 +12,30 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.scss']
 })
-export class UsuarioComponent implements OnInit, AfterViewInit {
+export class UsuarioComponent extends CrudComponent implements AfterViewInit {
 
   // seleçao
-  public sub;
-  public ids: number[];
-  public pos: number;
-  public novaPos: number;
   public registro: Usuario;
 
   constructor(
-    private _httpClient: HttpClient,
-    private _router: Router,
-    private _activatedRoute: ActivatedRoute
+    protected _httpClient: HttpClient,
+    protected _router: Router,
+    protected _activatedRoute: ActivatedRoute
   ) {
-  }
-
-  ngOnInit(): void {
-    this.sub = this._activatedRoute.paramMap.subscribe(params => {
-      let temp: string;
-      temp = params.get('ids');
-      if (!(/n/i).test(temp)) {
-        try {
-          this.ids = temp.split(',').map(e => Number.parseInt(e, 10));
-          temp = params.get('pos');
-          this.setPos(Number.parseInt(temp, 10));
-        } catch (e) {
-        }
-      }
-      if (!this.ids) {
-        this._router.navigate(['/pag', 'usuario']);
-      }
-      if (!this.pos) {
-        this.setPos(0);
-      }
-      this.novaPos = this.pos + 1;
-    });
+    super(_httpClient, _router, _activatedRoute, ['/pag', 'usuario']);
   }
 
   ngAfterViewInit() {
-    this._activatedRoute.url.subscribe(url => {
-      this._httpClient.get<Usuario>('http://localhost:8080/usuario/' + this.id).subscribe(
-        registro => {
-          this.registro = registro;
-        }
-      );
-    });
-  }
-
-  get id() {
-    if (!this.ids || !Array.isArray(this.ids)) return null;
-    if (!this.pos) { this.setPos(0); }
-    if (this.pos >= this.ids.length) { this.setPos(this.ids.length - 1); }
-    return this.ids[this.pos];
-  }
-
-  public vaiParaPrimeiro(): void {
-    this.vaiPara(0);
-  }
-
-  public vaiParaAnterior(): void {
-    this.vaiPara(this.pos - 1);
-  }
-
-  public vaiParaProximo(): void {
-    this.vaiPara(this.pos + 1);
-  }
-
-  public vaiParaUltimo(): void {
-    this.vaiPara(this.ids.length - 1);
-  }
-
-  public vaiPara(pos): void {
-    pos = ("" + pos).trim();
-    console.log(pos);
-    if ((pos || pos == 0) && !isNaN(pos)) {
-      this._router.navigate(['/pag', 'usuario', this.ids.join(), pos]);
+    console.log(1);
+    if (this.id) {
+      this._activatedRoute.url.subscribe(url => {
+        this._httpClient.get<Usuario>('http://localhost:8080/usuario/' + this.id).subscribe(
+          registro => {
+            this.registro = registro;
+          }
+        );
+      });
     }
-  }
-
-  private setPos(vlr: number) {
-    if (vlr < 0) {
-      vlr = 0;
-    } else if (vlr >= this.ids.length) {
-      vlr = this.ids.length -1;
-    }
-    this.pos = vlr;
   }
 
 }
