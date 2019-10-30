64d72f7752a0a3af0089fe9dd8ae0303fefc0f50
Fernando Frazao <fernando.frazao@emater.df.gov.br>
2019-10-30 15:51:51 -0300

    Componente
    Adicionado biblioteca para componente de pressão de botões funcionar
    
    Signed-off-by: Fernando Frazao <fernando.frazao@emater.df.gov.br>

--- mob/src/app/veiculo/registro/registro/registro.module.ts
+++ mob/src/app/veiculo/registro/registro/registro.module.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
-import { LongPressModule } from 'ionic-long-press';
+import { LongPressModule } from 'ionic-long-press/dist';
 
 @NgModule({
     declarations: [RegistroComponent],
