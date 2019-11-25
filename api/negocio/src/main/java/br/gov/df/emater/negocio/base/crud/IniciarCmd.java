e9d8f95922b5a515456cb6755c1b9b00ffbe7335
ffrazao@gmail.com <ffrazao@gmail.com>
2019-11-25 01:39:06 -0300

    ajustes
    Signed-off-by: ffrazao@gmail.com <ffrazao@gmail.com>

---
+++ api/negocio/src/main/java/br/gov/df/emater/negocio/base/crud/IniciarCmd.java
@@ -0,0 +1,27 @@
+package br.gov.df.emater.negocio.base.crud;
+
+import org.springframework.stereotype.Component;
+
+import br.com.frazao.cadeiaresponsabilidade.Comando;
+import br.com.frazao.cadeiaresponsabilidade.Contexto;
+
+@Component
+public class IniciarCmd extends Comando {
+
+	@Override
+	protected <k, v> void procedimento(Contexto<k, v> contexto) throws Exception {
+		Object modelo = contexto.getRequisicao();
+		Object instancia = null;
+		if (modelo != null) {
+
+			if (modelo instanceof Integer) {
+				// recuperar pelo id
+			} else {
+
+			}
+		}
+		instancia = modelo == null ? ((Class<?>) contexto.get("entidade")).newInstance() : modelo;
+		contexto.setResposta(instancia);
+	}
+
+}
